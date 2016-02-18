66847d2af2a04446ca5bb405cd60b2f301e64d3e
oskar202 <liblikoskar@gmail.com>
2016-02-18 09:47:16 +0200

    final

--- src/com/ttu/decathlon/Main.java
+++ src/com/ttu/decathlon/Main.java
@@ -21,7 +21,7 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, ArrayIndexOutOfBoundsException {
 
-        TreeMap<String, Integer> pointsList = new TreeMap<String, Integer>();
+        TreeMap<String, Integer> pointsList = new TreeMap<>();
 
         try {
             Scanner scanner = new Scanner(new File("decathlon.csv"));
